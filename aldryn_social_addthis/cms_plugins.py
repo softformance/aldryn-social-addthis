38d6cc4be6edeeab09e0bafe236f49a82f415ed5
Michał Ociepka <michal@ociepka.info>
2014-02-27 15:59:43 +0100

    Add links plugin, improve existing template

--- aldryn_social_addthis/cms_plugins.py
+++ aldryn_social_addthis/cms_plugins.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.utils.translation import ugettext_lazy as _
+from django.templatetags.static import static
 
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
@@ -44,3 +45,24 @@ class MailPlugin(CMSPluginBase):
         return context
 
 plugin_pool.register_plugin(MailPlugin)
+
+
+class LinksPlugin(CMSPluginBase):
+
+    model = Links
+    name = _('Social links')
+    render_template = 'aldryn_social_addthis/plugins/links.html'
+    ICON_URL = 'aldryn_social_addthis/icons/%(network)s_link.png'
+
+    def render(self, context, instance, placeholder):
+        context['instance'] = instance
+        links = []
+        for network_name in Links.AVAILABLE_LINKS:
+            social_url = getattr(instance, network_name)
+            if social_url:
+                icon_url = static(self.ICON_URL % {'network': network_name})
+                links.append((network_name, social_url, icon_url))
+        context['links'] = links
+        return context
+
+plugin_pool.register_plugin(LinksPlugin)
